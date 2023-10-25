f5f07fee33e1a7128fad484d1180da03f6ea4e4b
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-25 12:48:48 -0500

    reset some style rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 import Header from './components/Header'
+import { useState } from 'react'
 
 function App() {
+  const [projects, setProjects] = useState([])
   const [item, setItem] = useState({
     title: '',
     decription: '',
